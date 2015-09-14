bfc2f0991a88e4f6effea3e3c4b48d4053d5353c
Kevin Wells <darkoppressor@gmail.com>
2015-09-14 01:33:56 -0500

    Added version.h

---
+++ version.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2012 Cheese and Bacon Games, LLC */
+/* This file is licensed under the MIT License. */
+/* See the file docs/LICENSE.txt for the full license text. */
+
+#ifndef version_h
+#define version_h
+
+namespace Version{
+	//Build date
+	static const char YEAR[]="2015";
+	static const char MONTH[]="05";
+	static const char DAY[]="28";
+
+	//Version
+	static const int MAJOR=2;
+	static const int MINOR=0;
+	static const int MICRO=0;
+
+	//Status
+	static const char STATUS[]="";
+}
+
+#endif
